bfe1f7da9a4c8212b6d8f853f4c060533dac3366
melihtahaoz <melihtahaoz@gmail.com>
2022-06-17 14:17:30 -0400

    Voter login redirection is added

--- blockchain_evoting/e_voting/urls.py
+++ blockchain_evoting/e_voting/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "e_voting"
 urlpatterns = [
     path('',views.index, name='index'),
-    path('admin/', views.login_admin, name='login_admin')
+    path('admin/', views.login_admin, name='login_admin'),
+    path('login', views.login_voter, name='login_voter'),
+    path('loginPage/', views.loginPage, name='loginPage'),
 ]
--- blockchain_evoting/e_voting/views.py
+++ blockchain_evoting/e_voting/views.py
@@ -7,14 +7,24 @@ from django.contrib import messages
 def index(request):
     return render(request, 'home.html')
 
-def login(request):
-    if request.method == 'POST':
+def loginPage(request):
+    return render(request, 'login.html')
+
+def login_voter(request):
+    if request.method=='POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            return redirect('index')
-    else:  
-        form = AuthenticationForm()
-    return render(request, 'login.html')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('/login')
+            else:
+                messages.error(request,"Invalid username or password")
+        else:
+            messages.error(request,"Invalid username or password")
+    return render(request, 'home.html',context={'form':AuthenticationForm()})
 
 def login_admin(request):
     if request.method=='POST':
