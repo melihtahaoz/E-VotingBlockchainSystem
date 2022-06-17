ac649662f2aedfcb35cb9e138527333678a01e1c
melihtahaoz <melihtahaoz@gmail.com>
2022-06-17 19:28:20 -0400

    Admin panel updated

--- blockchain_evoting/e_voting/admin.py
+++ blockchain_evoting/e_voting/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+#admin.site.register(models.Candidate)
+
+
+
+
+
+class VoterAdmin(admin.ModelAdmin):
+
+    list_display = ('public_e_sign_key', 'has_voted')
+
+    # Disable add permission
+    def has_add_permission(self, request):
+        return False
+
+    # Disable delete permission
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(models.Voter,VoterAdmin)
+
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'party','vote_count')
+
+admin.site.register(models.Candidate, CandidateAdmin)
--- blockchain_evoting/e_voting/models.py
+++ blockchain_evoting/e_voting/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Voter(models.Model):
+    u_name = models.CharField(max_length=25,primary_key=True)
+    mail_addr = models.CharField(max_length=50)
+    has_voted = models.BooleanField(default=False,editable=False)
+    public_e_sign_key = models.CharField(max_length=350, default='test')
+
+class Candidate(models.Model):
+    name = models.CharField(max_length=25)
+    vote_count = models.IntegerField(default=0,editable=False)
+    party = models.CharField(max_length=25)
+
