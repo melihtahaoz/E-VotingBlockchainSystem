6599a9b2e7213bd97c55e78e70c4933b6d9d2aa4
Ege Erdogan <erdoganege@sabanciuniv.edu>
2022-06-18 16:17:29 +0300

    signup panel added

--- blockchain_evoting/e_voting/views.py
+++ blockchain_evoting/e_voting/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.admin.forms import AuthenticationForm
-from django.contrib.auth import login, logout,authenticate
+from django.contrib.auth import login, logout, authenticate
+from django.views.generic import CreateView
 from django.contrib import messages
+from .forms import VoterSignUpForm
+from .models import Candidate, Voter
 
 def index(request):
     return render(request, 'home.html')
 
 def loginPage(request):
     return render(request, 'login.html')
+
+def log_out(request):
+    logout(request)
+    return render(request, 'home.html')
+
+def voter_main(request):
+    candidate_list = Candidate.objects.all()
+    return render(request, 'voter_main.html', {'candidate_list': candidate_list})
 
 def login_voter(request):
     if request.method=='POST':
@@ -24,7 +35,7 @@ def login_voter(request):
                 messages.error(request,"Invalid username or password")
         else:
             messages.error(request,"Invalid username or password")
-    return render(request, 'home.html',context={'form':AuthenticationForm()})
+    return render(request, 'registration/login.html',context={'form':AuthenticationForm()})
 
 def login_admin(request):
     if request.method=='POST':
@@ -41,3 +52,18 @@ def login_admin(request):
         else:
             messages.error(request,"Invalid username or password")
     return render(request, 'home.html',context={'form':AuthenticationForm()})
+
+class voter_register(CreateView):
+    model = Voter  
+    form_class = VoterSignUpForm
+    template_name= 'voter_register.html'
+    def form_valid(self, form):
+        voter = form.save()
+        new_voter = Voter()
+        new_voter.u_name = form.cleaned_data.get('username')
+        new_voter.mail_addr = form.cleaned_data.get('mail_addr')
+        new_voter.save()
+
+        login(self.request, voter)
+        return redirect('/voter_main')
+
